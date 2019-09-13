e178e61a6cfcf13270c136308e683e2953076753
Magnific7 <mjones6944@gmail.com>
2019-09-13 09:24:52 +0200

    so far

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class Sources:
+    '''
+    Source class to define source objects.
+    '''
+    def __init__(self,id,name,description,url,category):
+          '''
+          Function to initialise source objects.
+          '''
+          self.id = id
+          self.name = name
+          self.description = description
+          self.url = url
+          self.category = category
+          
